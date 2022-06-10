b1041d6708515685047a47a49c05f52a0abb8541
angeloorru <angeloorru18@gmail.com>
2022-06-10 12:06:08 +0100

    Replaced Switch component with Routes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";import { Home } from "./pages/Home/Home";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Home } from "./pages/Home/Home";
 
 const App = () => {
     return (
