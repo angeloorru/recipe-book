9f7d664ae4bee95c574e2f9c5a78bca226851c71
angeloorru <angeloorru18@gmail.com>
2022-06-16 19:09:14 +0100

    Added Cypress tests

--- e2e/cypress/integration/recipe_app_spec.ts
+++ e2e/cypress/integration/recipe_app_spec.ts
@@ -5,7 +5,17 @@ describe("Recipe tests", () => {
       And measurements
       And cooking method
       Then the new recipe is saved for later`, () => {
-    expect(true).to.eq(false);
+    cy.visit('http:localhost:3000');
+    cy.get('.tab-2').click();
+    cy.url().should('include', '/home')
+    cy.get('#standard-basic').type('TestNewRecipe').should('have.value', 'TestNewRecipe');
+    cy.get('.add-button').click();
+    cy.get('.tab-3').click();
+
+    const cardContent = cy.get('.TestNewRecipe').get('.MuiCardHeader-content').last();
+    cardContent.should('have.text', 'TestNewRecipe');
+
+    cy.get('.expand-card').last().click();
   });
 
   it(`Given I want to look for a recipe
@@ -13,14 +23,43 @@ describe("Recipe tests", () => {
       Then I find the recipe
       And I can see the ingredients
       And I can see the cooking methods`, () => {
-    expect(true).to.eq(false);
+    cy.get('.tab-1').click();
+    cy.get('#standard-basic').type('TestNewRecipe').should('have.value', 'TestNewRecipe');
+    cy.get('.search-button').click();
+
+    //Check that a result is returned
+    const cardContent = cy.get('.TestNewRecipe').get('.MuiCardHeader-content').last();
+    cardContent.should('have.text', 'TestNewRecipe');
+    //Remove the card - All done
+    //cy.get('.delete-icon').click();
   });
 
-  it(`Given I want to look for a recipe by ingredients
+  it(`Given I delete a recipe
+      When I search by the name of the recipe
+      Then I do not find the recipe`, () => {
+    cy.get('.tab-1').click();
+    //Clear the txt field
+    cy.get('#standard-basic').clear();
+    cy.get('#standard-basic').should('have.value', '');
+
+    //Search for it again
+    cy.get('#standard-basic').type('TestNewRecipe').should('have.value', 'TestNewRecipe');
+    cy.get('.search-button').click();
+    cy.get('.TestNewRecipe').get('.MuiCardHeader-content').last().should('have.text', 'TestNewRecipe');
+    //Click the delete icon
+    cy.get('.delete-icon').last().click();
+    //Search for the recipe again
+    //Verify its deletion
+    cy.get('#standard-basic').type('TestNewRecipe');
+    cy.get('.search-button').click();
+    cy.get('.TestNewRecipe').should('not.exist');
+  });
+
+ /*  it(`Given I want to look for a recipe by ingredients
       When I search by the ingredient of the recipe
       Then I find the recipe
       And I can see the ingredients
       And I can see the cooking methods`, () => {
-    expect(true).to.eq(false);
-  });
+   // expect(true).to.eq(false);
+  }); */
 });
